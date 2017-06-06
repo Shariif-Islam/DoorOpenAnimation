8ba4bd5f67ad1b2b2cf07bcfc808f4ec5ff4529d
Shariif Islam <islamshariif@gmail.com>
2017-06-06 13:07:58 +0600

    push to main

--- DoorOpenAnimation/ViewController.swift
+++ DoorOpenAnimation/ViewController.swift
@@ -9,15 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var btn_open: UIButton!
+    @IBOutlet weak var rightViewCons: NSLayoutConstraint!
+    @IBOutlet weak var leftViewCons: NSLayoutConstraint!
+    
+    @IBOutlet weak var view_left: UIView!
+    @IBOutlet weak var view_right: UIView!
+    
+    var doorOpen = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func open(_ sender: Any) {
+        
+        openDoor()
+    }
+    
+    func openDoor() {
+    
+        self.view.layoutIfNeeded()
+        
+        UIView.animate(withDuration: 1.0, animations: {
+            
+            self.leftViewCons.constant = self.doorOpen ? -16 : -self.view_left.frame.size.width
+             self.rightViewCons.constant = self.doorOpen ? -16 : -self.view_right.frame.size.width
+            
+            self.view.layoutIfNeeded()
+            
+        }) { (success) in
+            
+            if self.doorOpen {
+            
+                self.doorOpen = false
+                self.btn_open.setTitle("open", for: .normal)
+            }
+            else {
+            
+                self.doorOpen = true
+                self.btn_open.setTitle("close", for: .normal)
+            }
+            
+        }
     }
 
 
